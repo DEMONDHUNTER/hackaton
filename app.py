6fbc0ac394d50d3e5eb01ec0933d2c186968ad91
mategri <mariatgrino.97@gmail.com>
2022-09-17 23:25:10 +0200

    agregue prueba 2

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+app.route('/prueba2')
+def index2():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
